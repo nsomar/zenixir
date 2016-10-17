1e5281cd434c50634aa1700d661685abecbb9984
Erik Nilsen <enilsen16@live.com>
2016-10-17 16:22:28 -0700

    use excoveralls instead

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule NewTest.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     test_coverage: [tool: Coverex.Task, coveralls: true],
+     test_coverage: [tool: ExCoveralls],
+     preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
      deps: deps]
   end
 
@@ -39,7 +40,7 @@ defmodule NewTest.Mixfile do
       {:poison, "~> 2.0"},
       {:exprintf, github: "parroty/exprintf"},
       {:exvcr, "~> 0.7", only: :test},
-      {:coverex, "~> 1.4.7", only: :test},
+      {:excoveralls, "~> 0.5", only: :test},
       {:inch_ex, ">= 0.0.0", only: :docs},
       {:ex_doc, "~> 0.12", only: :dev}
     ]
