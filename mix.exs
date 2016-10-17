4a9882e6ff5e88898807461cfaf0894387f75807
Erik Nilsen <enilsen16@live.com>
2016-10-17 15:24:22 -0700

    bump elixir requirement

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NewTest.Mixfile do
   def project do
     [app: :zenixir,
      version: "0.0.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      test_coverage: [tool: Coverex.Task, coveralls: true],
